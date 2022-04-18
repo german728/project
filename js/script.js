b5dd8b052adb99a5d795a83beba4ba729a853d78
German <german728@yandex.ru>
2022-04-18 15:57:14 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let inct = 10,
+    decr = 10;
+
+// inct++;
+// decr--;
+
+console.log(++inct);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 * 4 == 8);
+
+const   isCheked = true,
+        isClose = true;
+
+console.log(isCheked && isClose);
